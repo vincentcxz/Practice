af0f3e7e52f363aa2a95ec78e266b11d5778bceb
vincentcxz <vincentcxz@gmail.com>
2016-01-11 13:54:49 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+void printNameCourse();
+float computeCircleArea(float functionRadius);
+
+int main () {
+	printNameCourse();
+       
+       /**********************/
+    
+float area;
+
+float radius;
+
+radius = 10;
+
+area = computeCircleArea(radius);
+
+printf("The area is %f\n", area);
+
+return EXIT_SUCCESS;
+}
+
+void printNameCourse(){
+	puts("Vincent Concepcion");
+	puts("Hello EC72");
+	puts("5th compile!");
+}
+
+float computeCircleArea(float functionRadius){
+	return PI * functionRadius * functionRadius;
+}
